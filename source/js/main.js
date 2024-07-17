0059fdd87c3e3d8cc8f70d7f762a02cdbba3f8f9
CrashStatic <thecrashstatic@gmail.com>
2024-07-17 11:31:40 +0300

    Настраивает валидацию формы в блоке form

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import { initVideo } from './video.js';
 import { initTabs } from './tabs.js';
 import { initSwiperJuri, initSwiperReviews } from './swiper.js';
 import { initFaqAccordion, faqInitTabs } from './faq.js';
+import { initForm } from './form.js';
 
 initVideo();
 initTabs();
@@ -9,3 +10,4 @@ initSwiperJuri();
 initSwiperReviews();
 initFaqAccordion();
 faqInitTabs();
+initForm();
